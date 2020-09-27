67eacca4edddfe7f4a4abae920a72506f70ca1fb
Mazen <45051211+mmeziad@users.noreply.github.com>
2020-09-27 12:30:10 -0400

    updated scan

--- scan.cpp
+++ scan.cpp
@@ -2,19 +2,75 @@
 #include <string>
 #include <locale>
 #include <iostream>
+#include <fstream>
 #include "scanpp.h"
+
 
 char token_image[MAX_TOKEN_LEN];
 
+/*
+    The problem is that reading from a file doesn't save, which line we were last at,
+    because we call scan from different places instead of just one
+
+    We also don't want to pass that information every time we scan.
+
+    How can we scan from a file, line by line while knowing which line we were at before?
+    
+    Note:
+    There doesn't seem to be a built in method for this exactly
+
+    Suggestions: 
+    1) parse file into arrays or maps and then save what column you were at in the array
+            This will help with printing, which line the error occured on
+    2) Make scanner get as input, which line it was last on
+            Line by line might be fine, but this will cause problems for each char in a line
+    3)Make an in between function that gets called when you wanna scan smth and keep 
+    track of counter there
+
+
+
+
+*/
+
+
+
 token scan() {
-    static char c = ' ';
+      static char c = ' ';
+    //                              TRY READING FROM INSIDE THIS FUNCTION
+
+
+    // std::string line;
+    //std::ifstream file("test2");
+    // file.open("test2");
+
+
+    // if(file){
+    //     file.getc();
+    //     std::cout<<c<<"\n";
+    // }
+    // getline(inFile,line);
+    // std::cout<<line<<"\n";
+
+    // std::string line;
+    // if(inFile.is_open()){
+    //     while (getline(inFile,line)){
+    //         std::cout << line << "\n";
+    //     }
+    //     inFile.close();
+    // }else std::cout << "Problem opening file\n"; 
+
+  
         /* next available char; extra (int) width accommodates EOF */
     int i = 0;              /* index into token_image */
     /* skip white space */
     while (isspace(c)) {
+
         std::cin.get(c);
+        //std::cout<<c<<"\n";
     	//c = getchar();
     }
+
+
 
     if (c == EOF)
         return t_eof;
@@ -80,18 +136,18 @@ token scan() {
             //if ((c = getchar()) == '>') {
             //    c = getchar();
                 return t_neq;
-            } 
+            }
             
             // else if ((c = getchar()) == '=') {
             //     c = getchar();
             
             else if ((c) == '=') {
                 std::cin.get(c);
-                
+
                 return t_lte;
-            } else {
+            }else {
                 // TODO : should return an error?
-                std::cin.get(c);
+                //std::cin.get(c);
             	return t_lt;
             }
             break;
@@ -102,7 +158,7 @@ token scan() {
                 std::cin.get(c);
             	return t_gt;
             } else {
-                std::cin.get(c);
+                //std::cin.get(c);
                 return t_gte;
             }
             break;
